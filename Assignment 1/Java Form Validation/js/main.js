65f5ac2ec49a0efb69fe56ee8aab190359e36a6e
teejil <89534648+teejil@users.noreply.github.com>
2021-10-18 15:56:18 +0530

    Add files via upload

---
+++ Assignment 1/Java Form Validation/js/main.js
@@ -0,0 +1,111 @@
+// Defining a function to display error message
+function printError(elemId, hintMsg) {
+    document.getElementById(elemId).innerHTML = hintMsg;
+}
+
+// Defining a function to validate form 
+function validateForm() {
+    // Retrieving the values of form elements 
+    var firstname = document.contactForm.firstname.value;
+    var lastname = document.contactForm.lastname.value;
+    var email = document.contactForm.email.value;
+    var mobile = document.contactForm.mobile.value;
+    var city = document.contactForm.city.value;
+    var gender = document.contactForm.gender.value;
+    var hobbies = [];
+    var checkboxes = document.getElementsByName("hobbies[]");
+    for(var i=0; i < checkboxes.length; i++) {
+        if(checkboxes[i].checked) {
+            // Populate hobbies array with selected values
+            hobbies.push(checkboxes[i].value);
+        }
+    }
+    
+	// Defining error variables with a default value
+    var fnameErr = lnameErr = emailErr = mobileErr = cityErr = genderErr = hobbiesErr = true;
+    
+    // Validate name
+    if(firstname == "") {
+        printError("fnameErr", "Please enter your first name");
+    } else {
+        var regex = /^[a-zA-Z\s]+$/;                
+        if(regex.test(firstname) === false) {
+            printError("fnameErr", "Please enter a valid name");
+        } else {
+            printError("fnameErr", "");
+            fnameErr = false;
+        }
+    }
+    if(lastname == "") {
+        printError("lnameErr", "Please enter your last name");
+    } else {
+        var regex = /^[a-zA-Z\s]+$/;                
+        if(regex.test(lastname) === false) {
+            printError("lnameErr", "Please enter a valid name");
+        } else {
+            printError("lnameErr", "");
+            lnameErr = false;
+        }
+    }
+    // Validate email address
+    if(email == "") {
+        printError("emailErr", "Please enter your email address");
+    } else {
+        // Regular expression for basic email validation
+        var regex = /^\S+@\S+\.\S+$/;
+        if(regex.test(email) === false) {
+            printError("emailErr", "Please enter a valid email address");
+        } else{
+            printError("emailErr", "");
+            emailErr = false;
+        }
+    }
+    
+    // Validate mobile number
+    if(mobile == "") {
+        printError("mobileErr", "Please enter your mobile number");
+    } else {
+        var regex = /^[1-9]\d{9}$/;
+        if(regex.test(mobile) === false) {
+            printError("mobileErr", "Please enter a valid 10 digit mobile number");
+        } else{
+            printError("mobileErr", "");
+            mobileErr = false;
+        }
+    }
+    
+    // Validate city
+    if(city == "Select") {
+        printError("cityErr", "Please select your city");
+    } else {
+        printError("cityErr", "");
+        cityErr = false;
+    }
+    
+    // Validate gender
+    if(gender == "") {
+        printError("genderErr", "Please select your gender");
+    } else {
+        printError("genderErr", "");
+        genderErr = false;
+    }
+    
+    // Prevent the form from being submitted if there are any errors
+    if((fnameErr || lnameErr || emailErr || mobileErr || cityErr || genderErr) == true) {
+       return false;
+    } else {
+        // Creating a string from input data for preview
+        var dataPreview = "You've entered the following details: \n" +
+                          "First Name: " + firstname + "\n" +
+                          "Last Name: " + lastname + "\n" +
+                          "Email Address: " + email + "\n" +
+                          "Mobile Number: " + mobile + "\n" +
+                          "Country: " + country + "\n" +
+                          "Gender: " + gender + "\n";
+        if(hobbies.length) {
+            dataPreview += "Hobbies: " + hobbies.join(", ");
+        }
+        // Display input data in a dialog box before submitting the form
+        alert(dataPreview);
+    }
+};
